2c12fe1e93ddb0a9d95c23a79801ed7ea4de651c
Sergio Ruiz <sergioruizrock@gmail.com>
2024-01-31 16:09:19 -0300

    se agrego el select pais y parte de su logica para obtener las opciones que el resultado le brinda

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,6 @@ function App() {
     consultarAPI();
   }, []);
 
-
   const consultarAPI = async () => {
     console.log("ingreso");
     //setMostrarTarget(false);
@@ -27,39 +26,82 @@ function App() {
     }
   };
 
-  const definirCategorias=()=>{
-    if(resultados!==null || resultados.length!==0){
-      const arrayCategorias = resultados.map(o=>o.category); // con repetidos
-      const arraySinRep=[];
+  const definirCategorias = () => {
+    if (resultados !== null || resultados.length !== 0) {
+      const arrayCategorias = resultados.map((o) => o.category); // con repetidos
+      const arraySinRep = [];
       for (const elemento of arrayCategorias) {
         if (!arraySinRep.includes(elemento[0])) {
           arraySinRep.push(elemento[0]);
         }
       }
       return arraySinRep;
-    }else{
+    } else {
       return [];
     }
-  }
+  };
 
-  const categorias=definirCategorias();
+  const categorias = definirCategorias();
+
+
+  const definirPais = () => {
+    if (resultados !== null || resultados.length !== 0) {
+      const arrayPais = resultados.map((o) => o.country); // con repetidos
+      const arraySinRep = [];
+      for (const elemento of arrayPais) {
+        if (!arraySinRep.includes(elemento[0])) {
+          arraySinRep.push(elemento[0]);
+        }
+      }
+      return arraySinRep;
+    } else {
+      return [];
+    }
+  };
+
+  const pais = definirPais();
+
 
   return (
     <>
       <header className="bg-dark py-3">
         <h1 className="text-light text-center">React NodeTicias</h1>
         <div className="row d-flex pt-3">
-          <h4 className="text-light text-center mx-3 col-12 col-sm-12 col-md-1 col-lg-1">
-            Categoria
-          </h4>
-          <select className="mx-3 col-12 col-sm-12 col-md-4 col-lg-4" name="categoria" id="categoria">
-            <option value="">Elegir</option>
-            {
-              categorias.map((cat,pos)=>(
-                <option key={pos} value={cat}>{cat}</option>
-              ))
-            }
-          </select>
+          <div className="row d-flex pt-3 col-12 col-sm-12 col-md-6 col-lg-6">
+            <h4 className="text-light text-center mx-3 col-12 col-sm-12 col-md-2 col-lg2">
+              Categoria
+            </h4>
+            <select
+              className="mx-3 col-12 col-sm-12 col-md-8 col-lg-8"
+              name="categoria"
+              id="categoria"
+            >
+              <option value="">Elegir</option>
+              {categorias.map((cat, pos) => (
+                <option key={pos} value={cat}>
+                  {cat}
+                </option>
+              ))}
+            </select>
+          </div>
+          
+          <div className="row d-flex pt-3 col-12 col-sm-12 col-md-6 col-lg-6">
+            <h4 className="text-light text-center mx-3 col-12 col-sm-12 col-md-2 col-lg2">
+              Pais
+            </h4>
+            <select
+              className="mx-3 col-12 col-sm-12 col-md-8 col-lg-8"
+              name="categoria"
+              id="categoria"
+            >
+              <option value="">Elegir</option>
+              {pais.map((country, pos) => (
+                <option key={pos} value={country}>
+                  {country}
+                </option>
+              ))}
+            </select>
+          </div>
         </div>
       </header>
       <Container className="mainContainer">
